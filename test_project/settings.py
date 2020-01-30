4dfbe249e3e137a415f0161e80f551f9511c6892
wenyue gu <wg74@duke.edu>
2020-01-30 00:01:55 -0500

    environment

--- test_project/settings.py
+++ test_project/settings.py
@@ -157,3 +157,12 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+
+ENVIRONMENT = os.environ.get('ENVIRONMENT', default='production')
+
+
+if ENVIRONMENT == 'production':
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
